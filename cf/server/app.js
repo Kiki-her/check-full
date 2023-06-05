f6652010131da8cd868022c0f282f648ea50e0b8
kiki <k.kazaneko@gmail.com>
2023-06-05 17:34:47 +0900

    add app.js code

--- cf/server/app.js
+++ cf/server/app.js
@@ -1,21 +1,29 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 //確認
 
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+app.listen(port);
+if (port === 3000) {
+  console.log(`Now hosting at "http://localhost:${port}/"`);
+}
+// app.use(express.static(path.join(__dirname, "build")));
+
+// app.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 app.get("/api", (req, res) => {
   res.send("Hi~👋🏻");
 });
-
-app.listen(9000);
 
 module.exports = app;
