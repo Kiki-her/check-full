a48788248e32e36f9f29f82cd429384fb62c8095
kiki <k.kazaneko@gmail.com>
2023-06-05 11:49:42 +0900

    add shopping cart badge component

--- cf/src/App.js
+++ cf/src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
-// import Table from "./components/Table";
-import Checkout from "./components/Checkout";
+import Table from "./components/Table";
+// import Checkout from "./components/Checkout";
+import ShopIcon from "./components/ShopIcon";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ShopIcon />
         <h1>Hi all!</h1>
-        <Checkout />
+        <Table />
       </header>
     </div>
   );
