b7449addc2e425023f112f39013d5d92bef6b3cd
kiki <k.kazaneko@gmail.com>
2023-06-06 14:53:02 +0900

    add environment

--- cf/server/app.js
+++ cf/server/app.js
@@ -2,24 +2,31 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = process.env.PORT || 3000;
+const knex = require("./knex");
 
 app.use(express.json());
 
 //確認
 
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, "build")));
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+app.get("/products", async (req, res) => {
+  const products = await knex
+    .select()
+    .from("products")
+    .then((results) => results);
+  res.send(products);
+});
+app.get("/api", (req, res) => {
+  res.send("Hi~👋🏻");
 });
 
 app.listen(port);
 if (port === 3000) {
   console.log(`Now hosting at "http://localhost:${port}/"`);
 }
-
-app.get("/api", (req, res) => {
-  res.send("Hi~👋🏻");
-});
 
 module.exports = app;
